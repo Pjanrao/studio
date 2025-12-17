8f7eb6c1e8af608b895ff34ff9a2d46d11f6144a
Pranjal Janrao <pranjal.janrao@gmail.com>
2025-12-17 10:09:10 +0000

    admin should not displayed admin's details in customer list

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,12 +5,14 @@ import { getUsers } from '@/lib/models/User';
 export async function GET(req: NextRequest) {
   try {
     const users = await getUsers();
-    // Omit passwords from the response
-    const usersWithoutPasswords = users.map(user => {
+    // Filter out admins and omit passwords from the response
+    const customers = users
+      .filter(user => user.role === 'User')
+      .map(user => {
         const { password, ...rest } = user;
         return rest;
     });
-    return NextResponse.json(usersWithoutPasswords);
+    return NextResponse.json(customers);
   } catch (error) {
     console.error('Failed to fetch users:', error);
     return NextResponse.json({ message: 'Failed to fetch users' }, { status: 500 });
